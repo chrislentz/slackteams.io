9706f29499e609ad5d2c99270e4745dcae7b9508
Chris Lentz <chris@atlchris.com>
2015-03-14 20:18:16 -0400

    Added a fixed logo header on scroll.

--- assets-src/js/main.js
+++ assets-src/js/main.js
@@ -1,6 +1,29 @@
+function toggleFixedMenu() {
+  var num = 100;
+
+  console.log($(window).scrollTop());
+
+  if ($(window).scrollTop() > num) {
+    if ($('.mini-logo').css('opacity') == 0) {
+      $('.mini-logo').animate({ opacity: 1.0 }, 500);
+    }
+  }
+  else {
+    if ($('.mini-logo').css('opacity') == 1) {
+      $('.mini-logo').animate({ opacity: 0 }, 500);
+    }
+  }
+}
+
 $(function() {
   var colors = ['blue', 'pink', 'yellow', 'green'];
   var randomColor = colors[Math.floor(Math.random() * colors.length)];
 
   $('body').addClass(randomColor);
+
+  toggleFixedMenu();
+
+  $(window).scroll(function() {
+    toggleFixedMenu();
+  });
 });
